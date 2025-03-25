eb2ab6f233c4551659d5f238640a8bf9ff490386
rvaleev451 <rvaleev@neoflex.ru>
2025-03-25 16:24:37 +0300

    Merge conflict refactoring.

--- components/blitz.key.manager/src/main/java/ru/neoflex/wso2/blitz/client/BlitzConstants.java
+++ components/blitz.key.manager/src/main/java/ru/neoflex/wso2/blitz/client/BlitzConstants.java
@@ -57,9 +57,8 @@ public class BlitzConstants {
 
     public static final String REGISTRATION_API_KEY = "API Token";
 
-    public static final String GRANT_TYPES_FIELD_NAME = "client_credentials";
-    public static final String BLITZ_API_SYS_APP = "blitz_api_sys_app";
-    public static final String BLITZ_API_SYS_APP_CHG = "blitz_api_sys_app_chg";
+    public static final String GRANT_TYPES_FIELD = "client_credentials";
+    public static final String SCORE_FIELD = "blitz_api_sys_app blitz_api_sys_app_chg";
 
     BlitzConstants() {
     }
