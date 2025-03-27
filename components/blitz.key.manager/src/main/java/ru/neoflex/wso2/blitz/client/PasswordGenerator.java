4bb5f998ec5eee7774227ab8841123a8a7681509
aldoroshenko <aldoroshenko@neoflex.ru>
2025-03-27 14:19:56 +0400

    upd

--- components/blitz.key.manager/src/main/java/ru/neoflex/wso2/blitz/client/PasswordGenerator.java
+++ components/blitz.key.manager/src/main/java/ru/neoflex/wso2/blitz/client/PasswordGenerator.java
@@ -1,14 +1,16 @@
 package ru.neoflex.wso2.blitz.client;
 
 import java.util.Random;
+import static ru.neoflex.wso2.blitz.client.BlitzConstants.AVAILABLE_SYMBOLS;
+import static ru.neoflex.wso2.blitz.client.BlitzConstants.PASSWORD_LENGTH;
 
 public class PasswordGenerator {
     private static final Random random = new Random();
 
     public static String generatePassword() {
         StringBuilder password = new StringBuilder();
-        for (int i = 0; i < BlitzConstants.PASSWORD_LENGTH; i++) {
-            password.append(getRandomChar(BlitzConstants.AVAILABLE_SYMBOLS));
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            password.append(getRandomChar(AVAILABLE_SYMBOLS));
         }
         return password.toString();
     }
